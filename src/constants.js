b83e5554526f1e6e7af8027b6621108eb7dae31e
Wenqi (Kiki) Gai <96741883+KikiGai@users.noreply.github.com>
2023-05-17 22:52:26 -0500

    Delete src directory

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-export const SAT_API_KEY = "YNLUEE-E5RFYB-ND3JXT-4WZ2";
-export const STARLINK_CATEGORY = "52";
-export const NEARBY_SATELLITE = "rest/v1/satellite/above";
-export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
-export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
-export const BASE_URL = "";
-
